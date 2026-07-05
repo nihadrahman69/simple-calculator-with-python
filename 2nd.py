be3534f5aea08e661f0ebda9190cbd392e0291a6
nihadrahman69 <rahmannih69@gmail.com>
2026-07-05 14:57:39 +0600

    Used match case instead of if-else and reaname file

--- 2nd.py
+++ 2nd.py
@@ -6,7 +6,23 @@ num2 = float(input("Enter second number: "))
 print("select operation (+,_,*,/,sin,cos): ")
 
 operation = input()
-if operation == '+':
+match operation:
+    case "+":
+        print(add(num1,num2))
+    case "-":
+        print(sub(num1,num2))
+    case "*":
+        print(mul(num1,num2))
+    case "/":
+        print(div(num1,num2))
+    case "sin":
+        print(m_sin(num1))
+        print(m_sin(num2))
+    case "cos":
+        print(m_cos(num1))
+        print(m_cos(num2))  
+  
+"""if operation == '+': 
     print(add(num1, num2))
 if operation == '-':
     print(sub(num1, num2))
@@ -19,4 +35,5 @@ if operation == 'sin':
     print(m_sin(num2))
 if operation == 'cos':
     print(m_cos(num1))
-    print(m_cos(num2))
+    print(m_cos(num2))"""
+
